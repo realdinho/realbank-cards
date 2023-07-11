079e782c3d3f543600531f0bbd210e8df67ca912
realdinho <realdo.dias3@gmail.com>
2023-07-11 16:57:54 +0800

    add correlation ID and update Docker image name

--- src/main/java/com/realdiv/cards/controller/CardController.java
+++ src/main/java/com/realdiv/cards/controller/CardController.java
@@ -5,19 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+
 import com.realdiv.cards.config.CardConfig;
 import com.realdiv.cards.model.Card;
 import com.realdiv.cards.model.Property;
 import com.realdiv.cards.repository.CardRepository;
 
 @RestController
-@RequestMapping("cards")
 public class CardController {
     
     @Autowired
@@ -28,6 +28,7 @@ public class CardController {
 
     @GetMapping("/{customerId}")
     public List<Card> getCardsList(
+        @RequestHeader("realbank-correlation-id") String correlationId,
         @PathVariable int customerId
     ) {
         List<Card> cards = cardRepository.findByCustomerId(customerId);
